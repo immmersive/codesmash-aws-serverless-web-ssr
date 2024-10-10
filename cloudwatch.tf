cc8ef8fb14138b1676278b8e7fc906317397efc7
Sarah Rollins <s.rollins751@gmail.com>
2024-10-10 12:24:41 +0900

    CloudWatch block moved

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -7,3 +7,10 @@ resource "aws_cloudwatch_event_target" "trigger_lambda" {
   rule      = aws_cloudwatch_event_rule.warm_lambda.name
   arn       = aws_lambda_function.lambda.arn
 }
+
+resource "aws_lambda_permission" "allow_cloudwatch" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.warm_lambda.arn
+}
